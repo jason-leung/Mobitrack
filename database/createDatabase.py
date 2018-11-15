8f5494b54a2f6859fe414edcb92ef4d65ae3f0fe
Jamie <jmkim0603@gmail.com>
2018-11-15 10:21:07 -0500

    Database: Minor edit for the base script
    1. Changes made so that the default password for root is now "password"
    2. Added comment
    3. Renamed the script to a more appropriate one

--- database/createDatabase.py
+++ database/createDatabase.py
@@ -1,20 +1,28 @@
+# This script create database and tables for mobitrack.
+
 import mysql.connector
 
 db = mysql.connector.connect (
 		host="localhost",
-		user="root", # Your username
-		passwd="" # Your pw
+		user="root", #yourusername
+		passwd="password" #yourpw
 	)
 
+
 mycursor = db.cursor()
+
+# Create database if it doesn't exist
 mycursor.execute("CREATE DATABASE IF NOT EXISTS mobitrack")
 mycursor.execute("USE mobitrack")
 
+# Create the wearing session table if it doesn't exist
 mycursor.execute("CREATE TABLE IF NOT EXISTS wearing_session (" +
 				 "SessionID VARCHAR(16), " +
 				 "PatientID VARCHAR(255), " +
 				 "Location VARCHAR(20), " + 
 				 "TimeStamp DATE )")
+				 
+# Create the exercise period table if it doesn't exist
 mycursor.execute("CREATE TABLE IF NOT EXISTS exercise_period (" +
 				 "PeriodID VARCHAR(16), " +
 				 "SessionID VARCHAR(16), " +
