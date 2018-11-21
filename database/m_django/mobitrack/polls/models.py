1c959ddbacf1064357b30819bed25f9f51178d77
Jamie <jmkim0603@gmail.com>
2018-11-21 01:13:56 -0500

    database: initializing mysql database with django
    Adding django connection with mysql database

--- database/m_django/mobitrack/polls/models.py
+++ database/m_django/mobitrack/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class WearingSession(models.Model):
+	sessionID = models.CharField(max_length=16, primary_key=True)
+	patientID = models.CharField(max_length=8)
+	location = models.CharField(max_length=20)
+	timeStamp = models.DateTimeField('date published')
+	
+class ExercisePeriod(models.Model):
+	periodID = models.CharField(max_length=8, primary_key=True)
+	sessionID = models.CharField(max_length=16)
+	duration = models.CharField(max_length=255)
+	repetitions = models.CharField(max_length=20)
+	timeStamp = models.DateTimeField('date published')
