fd5382c38980ab69499358e013b8f3ff3eed9ba1
Jamie <jmkim0603@gmail.com>
2018-11-21 15:16:57 -0500

    database: correcting the architecture
    Following the tutorial: https://docs.djangoproject.com/en/2.1/intro/tutorial02/

--- database/mobitrack/database/models.py
+++ database/mobitrack/database/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 class WearingSession(models.Model):
+	def __str__(self):
+		return self.sessionID
+		
 	sessionID = models.CharField(max_length=16, primary_key=True)
 	patientID = models.CharField(max_length=8)
 	location = models.CharField(max_length=20)
 	timeStamp = models.DateTimeField('date published')
 	
 class ExercisePeriod(models.Model):
+	def __str__(self):
+		return self.periodID
+		
 	periodID = models.CharField(max_length=8, primary_key=True)
 	sessionID = models.CharField(max_length=16)
 	duration = models.CharField(max_length=255)
