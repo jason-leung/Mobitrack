a60ba0740c9f40bc2a3173c31b8e20562508d228
jason-leung <leungj7049@gmail.com>
2018-11-21 17:55:40 -0500

    MFP demo
    - added LED feedback code for Arduino
    - added realtime segmentation method

--- misc/Data Collection/Arduino/imu_bluetooth/imu_bluetooth.ino
+++ misc/Data Collection/Arduino/imu_bluetooth/imu_bluetooth.ino
@@ -8,34 +8,53 @@ int16_t ax,ay,az, gx, gy, gz;
 
 // Set up state variables
 int16_t state = 0;
+int16_t ledPin = 3;
+unsigned long ledOnDuration = 500;
+unsigned long ledStartTime = 0;
 
 void setup() {
-    Wire.begin();
-    accelgyro.initialize();
-    accelgyro.setXGyroOffset(77);
-    accelgyro.setYGyroOffset(34);
-    accelgyro.setZGyroOffset(-24);
-    accelgyro.setXAccelOffset(-2390);
-    accelgyro.setYAccelOffset(-482);
-    accelgyro.setZAccelOffset(1188);
-    Serial.begin(115200);
+  // setup IMU
+  Wire.begin();
+  accelgyro.initialize();
+  accelgyro.setXGyroOffset(77);
+  accelgyro.setYGyroOffset(34);
+  accelgyro.setZGyroOffset(-24);
+  accelgyro.setXAccelOffset(-2390);
+  accelgyro.setYAccelOffset(-482);
+  accelgyro.setZAccelOffset(1188);
+  
+  // setup LED
+  pinMode(ledPin, OUTPUT);
+  // setup serial communication
+  Serial.begin(115200);
 }
 
 void loop() {
   // Get state
+  // 1 = start streaming and rep not detected
+  // 2 = start streaming and rep detected
   if(Serial.available())
   {
       state = Serial.read();
   }
 
-  // Stream IMU data if state = 1
+  // Rep detected
+  if(state == 50){
+    ledStartTime = millis();
+    digitalWrite(ledPin, HIGH);
+    state = 49;
+  }
+  if(millis() - ledStartTime >= ledOnDuration){
+    digitalWrite(ledPin, LOW);
+  }
+
+  // Stream IMU data
   if(state == 49){
     // Read data
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
     
     // Print data to serial
     // Format: ax, ay, az, gx, gy, gz
-    
     delay(10);
     Serial.println("");
     Serial.print(ax);
