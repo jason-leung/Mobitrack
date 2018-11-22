0740c3579864bcc820d21376d628ce216dd57498
andrea <asabo@uwaterloo.ca>
2018-11-21 21:26:28 -0500

    Update to realtimeProcessing

--- processing/realTimeProcessingTestScript.m
+++ processing/realTimeProcessingTestScript.m
@@ -1,7 +1,7 @@
 close all, clear all, clc
 % Load data
 %cd ('C:\Users\andre\OneDrive\School\4A\BME 461\Mobitrack\data\Nov17');
-filename = 'D:\OneDrive\School\4A\BME 461\Mobitrack\data\Nov17\RA_flx_full.mat';
+filename = 'D:\OneDrive\School\4A\BME 461\Mobitrack\data\Nov17\LA_flx_small.mat';
 data = load(filename);
 
 % Load Classifier
@@ -11,7 +11,12 @@ SVMModel = classifier.SVMModel;
 dataMatrix = [data.ax; data.ay; data.az; data.gx; data.gy; data.gz];
 time = data.time;
 
-processor = DataProcessor(SVMModel, 10);
+windowSize = 10;
+lag = 1000; % samples
+threshold = 0.8; % standard deviations
+influence = 1;
+
+processor = DataProcessor(SVMModel, windowSize, lag, threshold, influence);
 
 for i = 1:length(time)
     processor.processStep(dataMatrix(:,i), time(i));
@@ -25,5 +30,10 @@ end
 
 %%
 figure, hold on
-plot(time, processor.pitchSinceLastSegment * 180.0 / pi), title('Real-time processing pitch')
+plot(processor.pitchSinceLastSegment * 180.0 / pi), title('Real-time processing pitch')
+stairs(processor.signals * 5,'r','LineWidth',1.5);
+    for i = 1:size(processor.segmentInds,1)
+        rectangle('Position', [processor.segmentInds(i,1), min(processor.pitchSinceLastSegment), processor.segmentInds(i,2) - processor.segmentInds(i,1), max(processor.pitchSinceLastSegment) - min(processor.pitchSinceLastSegment)], 'EdgeColor', 'r');
+    end
+%segments = extractSegments(data.time, processor.rollSinceLastSegment, processor.pitchSinceLastSegment, processor.segmentInds);
 
