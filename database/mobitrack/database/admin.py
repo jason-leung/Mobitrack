6fa443e90b500380da4aa5246c43a59ac93d71df
Jamie <jmkim0603@gmail.com>
2018-11-22 10:19:40 -0500

    database: minor edit
    minor edit for displaying database on front end

--- database/mobitrack/database/admin.py
+++ database/mobitrack/database/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import WearingSession, ExercisePeriod
 
-admin.register(WearingSession)    
+admin.site.register(WearingSession)    
 admin.site.register(ExercisePeriod)
