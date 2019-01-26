465a111e2b3d462eae2874ebcad089b69fa24e4f
Jamie <jmkim0603@gmail.com>
2019-01-26 00:14:12 -0500

    Fixing home directory

--- database/mobitrack/mobitrack/urls.py
+++ database/mobitrack/mobitrack/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-	path('home/', include('database.urls')),
+	path('', include('database.urls')),
     path('admin/', admin.site.urls),
 ]
+
