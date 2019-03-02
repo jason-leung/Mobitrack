f78c881c9a652bddd1a042af529f9717407cfb56
asabo227 <andreasabo@yahoo.ca>
2019-03-02 09:11:20 -0500

    Celery proof of concept

--- database/mobitrack/frontend/views.py
+++ database/mobitrack/frontend/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect
 
-# from mobitrack.tasks import addAndrea
+from frontend.tasks import *
 
 from rest_framework import generics
 
@@ -27,7 +27,7 @@ def formSubmit(request):
         data = json.loads(request.body)
         print(data)
 
-        # addAndrea.delay(2, 34)
+        addAndrea.delay(2, 34)
 
 
 
